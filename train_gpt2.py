1dc6334de7cf77d87dfd12221ff82277493b7cf5
yezinri <yejinlee0707@gmail.com>
2024-09-29 09:43:10 +0000

    sampling init, prefix tokens, tokenization

--- train_gpt2.py
+++ train_gpt2.py
@@ -163,5 +163,17 @@ class GPT(nn.Module):
     
 # ----------------------------------------------------------------------------------------
 
+num_return_sequences = 5
+max_length = 30
+
 model = GPT.from_pretrained('gpt2')
-print("didn't crash yay!")
+model.eval()
+model.to('cuda')
+
+# prefix tokens
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
+x = tokens.to('cuda')
