622d84fdd42061fe467280d8bf66bd576dada80c
yezinri <yejinlee0707@gmail.com>
2024-10-01 04:02:20 +0000

    bfloat16, 415ms

--- train_gpt2.py
+++ train_gpt2.py
@@ -248,7 +248,8 @@ for i in range(50):
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    logits, loss = model(x, y)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
     torch.cuda.synchronize() # GPU 작업 완료 때까지 대기
