79448914942874c0e51723dbb4013bf872a6f029
yezinri <yejinlee0707@gmail.com>
2024-10-01 03:43:46 +0000

    Let's make it fast. 800ms

--- train_gpt2.py
+++ train_gpt2.py
@@ -220,6 +220,8 @@ class DataLoaderLite:
 # ----------------------------------------------------------------------------------------
 
 # attempt to autodetect the device
+import time
+
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
@@ -231,7 +233,7 @@ torch.manual_seed(1337)
 if torch.cuda.is_available():
     torch.cuda.manual_seed(1337)
 
-train_loader = DataLoaderLite(B=4, T=32)
+train_loader = DataLoaderLite(B=16, T=1024)
 
 # get logits
 model = GPT(GPTConfig())
@@ -240,13 +242,18 @@ model.to(device)
 # optimize!
 optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize() # GPU 작업 완료 때까지 대기
+    t1 = time.time()
+    dt = (t1 - t0) * 1000 # time difference in miliseconds
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
